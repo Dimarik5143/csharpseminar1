fc648dd72fb17aebbee10a94d7c565aa6898a18b
Dimarik <dimarik2003@gmail.com>
2023-10-12 20:35:47 +0300

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите число ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+if(number < 0)
+{
+    number = -number;
+}
+int numberNegative = -number;
+while(numberNegative <= number)
+{
+    System.Console.Write($"{numberNegative} ");
+    numberNegative++;
+}
