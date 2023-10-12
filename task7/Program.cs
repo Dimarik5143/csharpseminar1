2ba81c69045c88bddf806c42275c1adf9e9bc7e6
Dimarik <dimarik2003@gmail.com>
2023-10-12 20:52:59 +0300

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+if(number >= 100 && number <= 999)
+{
+    int digitlast = number % 10;
+    System.Console.Write(digitlast);
+}
+else
+{
+    System.Console.WriteLine("Введите трехзначное число");
+}
