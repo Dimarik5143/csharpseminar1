3ae20832d28afc925e5d1d554be06199c6c8b665
Dimarik <dimarik2003@gmail.com>
2023-10-12 19:43:56 +0300

    Start task01

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите два числа: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == x)
+{
+    System.Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    System.Console.WriteLine("Не является");
+}
