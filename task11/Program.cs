3dbfb8623ce13b909e5e84773ac1510aa59969c3
Dimarik <dimarik2003@gmail.com>
2023-10-16 20:49:13 +0300

    Task 9, 11, 12, 14, 16

---
+++ task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит любое
+// трёхзначное число и на выходе удаляет вторую
+// цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine(number);
+int number1 = number/100;
+int number3 = number%10;
+int number4 = number1*10+number3;
+System.Console.Write(number4); //System.Console.Write($"{number1}{number3}");
---
+++ task9/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+System.Console.WriteLine($"Наше случайное число = {number}");
+int digit1 = number/10; // 78/10 = 7,8 -> 7
+int digit2 = number%10; // 78%10 = 78/10 = 7.8 остается 8
+if(digit1 > digit2)
+{
+    System.Console.WriteLine(digit1);
+}
+else if(digit1 < digit2)
+{
+    System.Console.WriteLine(digit2);
+}
+else
+{
+    System.Console.WriteLine("Цтфры равны");
+}
