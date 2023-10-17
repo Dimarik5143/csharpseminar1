1f4f311e128e623f6732bc43077547166b45ea9b
Dimarik <dimarik2003@gmail.com>
2023-10-17 13:16:21 +0300

    Homework Task10, Task13, Task15

---
+++ task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 100 && number <= 999)
+        {
+            int secondDigit = (number / 10) % 10;
+            Console.WriteLine($"Вторая цифра числа {number} - это {secondDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Введенное число не является трехзначным.");
+        }
---
+++ task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 100)
+        {
+            while( number >= 1000 )
+            {
+                number = number / 10;
+            }
+            number = number % 10;
+            System.Console.WriteLine($"Третья цифра числа - это {number}");
+        }
+        else
+            {
+            Console.WriteLine("Третьей цифры нет.");
+            }
+            
