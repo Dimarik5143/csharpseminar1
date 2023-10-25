0bbf0a8bdd342780a89a17e2f7d6ebe4cb7dbf0b
Dimarik <dimarik2003@gmail.com>
2023-10-25 11:26:25 +0300

    task 19, 211, 23

---
+++ task19/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на
+// вход пятизначное число и проверяет, является ли оно
+// палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+static bool IsPalindrome(int number)
+    {
+        if (number < 10000 || number > 99999)
+        {
+            Console.Write("Число не пятизначное");
+            //Console.WriteLine("False");
+            return false;
+        }
+
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = number / 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
+        Console.Write("Введите пятизначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        bool isPalindrome = IsPalindrome(number);
+
+        if (isPalindrome)
+        {
+            Console.Write("True.");
+        }
+        else
+        {
+            Console.Write("False.");
+        }
---
+++ task211/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на
+// вход координаты двух точек и находит расстояние
+// между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+static double CalculateDistance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+    {
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+        double deltaZ = z2 - z1;
+
+        double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+        return distance;
+    }
+        Console.WriteLine("Введите координаты точки A (x1, y1, z1): ");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        double y1 = Convert.ToDouble(Console.ReadLine());
+        double z1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите координаты точки B (x2, y2, z2): ");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        double y2 = Convert.ToDouble(Console.ReadLine());
+        double z2 = Convert.ToDouble(Console.ReadLine());
+
+        double distance = CalculateDistance3D(x1, y1, z1, x2, y2, z2);
+        Console.WriteLine($"Расстояние между точкой A и точкой B: {distance:F2}");
